Median absolute deviation - Fix percentile calculation and refactor.
Fix the percentile calculation, which subtracted the mean of each value
before determining the value although it should have (and does now) take
the relative difference as is. Extended the method at the same time to
fetch quantile values for absolute and relative data values.
Replaced the internal caching hash with functools lru_cache.

raster_compare/base/median_absolute_deviation.py
@@ -1,3 +1,4 @@
+import functools
 import math
 
 import numpy as np
@@ -18,8 +19,7 @@ class MedianAbsoluteDeviation(object):
 
     def __init__(self, data):
         self._data = data
-        self._data_median = np.median(self.data)
-        self._percentiles = {}
+        self._data_median = np.median(self.data, overwrite_input=True)
 
     @property
     def data(self):
@@ -29,16 +29,13 @@ class MedianAbsoluteDeviation(object):
     def data_median(self):
         return self._data_median
 
-    @property
-    def median(self):
-        return self.percentile(50)
-
-    def standard_deviation(self, width=1):
-        return self.percentile(self.STANDARD_DEVIATIONS[width])
+    def standard_deviation(self, width=1, absolute=False):
+        return self.percentile(self.STANDARD_DEVIATIONS[width], absolute)
 
     def absolute_difference(self, a):
         return math.fabs(a - self.data_median)
 
+    @functools.lru_cache(1)
     def normalized(self):
         """
         NMAD from Höhle & Höhle, 2009
@@ -46,9 +43,11 @@ class MedianAbsoluteDeviation(object):
         absolute_difference = np.vectorize(self.absolute_difference)
         return self.MAD_CONSTANT * np.median(absolute_difference(self.data))
 
-    def percentile(self, percent):
-        if str(percent) not in self._percentiles:
-            absolute_difference = np.vectorize(self.absolute_difference)
-            self._percentiles[str(percent)] = \
-                np.percentile(absolute_difference(self.data), percent)
-        return self._percentiles[str(percent)]
+    @functools.lru_cache(16)
+    def percentile(self, percent, absolute=False):
+        if absolute:
+            data = np.abs(self.data)
+        else:
+            data = self.data
+
+        return np.percentile(data, percent, overwrite_input=True)