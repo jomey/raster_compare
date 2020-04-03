5b32382c6ffd29407e1b6405ba6055155febdacb
Joachim Meyer <j.meyer@utah.edu>
2020-04-02 20:13:12 -0600

    Setup.py - Add development requirements.
    Can be installed via `pip install -e .[dev]`

--- setup.py
+++ setup.py
@@ -4,14 +4,26 @@ setup(
     name='raster-compare',
     version='0.1',
     packages=[
-        'raster_compare.base', 'raster_compare.plots',
+        'raster_compare.base',
+        'raster_compare.plots',
     ],
     url='https://github.com/jomey/raster_compare',
     author='Joachim Meyer',
     author_email='j.meyer@utah.edu',
     description='Raster comparison tools',
     install_requires=[
-        'gdal', 'matplotlib', 'numpy', 'pandas',
-        'opencv-python', 'pillow', 'statsmodels'
-    ]
+        'gdal',
+        'matplotlib',
+        'numpy',
+        'opencv-python',
+        'pandas',
+        'pillow',
+        'statsmodels'
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'pytest',
+        ]
+    }
 )
