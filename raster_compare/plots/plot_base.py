37a266659a217a22999bb8be10fa99e0034730e2
Joachim Meyer <j.meyer@utah.edu>
2020-04-06 08:20:30 -0600

    PlotBase - rename internal variable.
    Better reflects information it is holding.

--- raster_compare/plots/plot_base.py
+++ raster_compare/plots/plot_base.py
@@ -125,9 +125,9 @@ class PlotBase(object):
     def insert_colorbar(plot, ax, data, label, **kwargs):
         legend = make_axes_locatable(ax)
         cax = legend.append_axes("right", size="5%", pad=0.05)
-        scale_bar = plot.colorbar(data, cax=cax, **kwargs)
-        scale_bar.set_label(label=label, rotation=270)
-        return scale_bar
+        color_bar = plot.colorbar(data, cax=cax, **kwargs)
+        color_bar.set_label(label=label, rotation=270)
+        return color_bar
 
     def print_status(self, message=''):
         status = 'Plotting ' + self.__class__.__name__
