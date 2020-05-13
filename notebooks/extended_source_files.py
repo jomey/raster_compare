6e619112585273ffd44cfeae49c3f25c50196d1f
Joachim Meyer <j.meyer@utah.edu>
2020-05-13 10:28:51 -0600

    Notebooks - set snow free and on raster to different band.
    Ensure we are reading band 3 for the snow free and on DEM since it will
    contain the idw value, when creating a GeoTiff with PDAL and adding min,
     max, idw and count statistics.

--- notebooks/extended_source_files.py
+++ notebooks/extended_source_files.py
@@ -6,10 +6,10 @@ from source_files import *
 HILLSHADE_SNOW_ON = dict(azimuth=100, altitude=47)
 HILLSHADE_SNOW_FREE = dict(azimuth=247, altitude=32)
 
-sfm_snow_free = RasterFile(SFM_SNOW_FREE, band_number=1)
+sfm_snow_free = RasterFile(SFM_SNOW_FREE, band_number=3)
 sfm_snow_free_values = sfm_snow_free.band_values()
 
-sfm_snow_on = RasterFile(SFM_SNOW_ON, band_number=1)
+sfm_snow_on = RasterFile(SFM_SNOW_ON, band_number=3)
 sfm_snow_on_values = sfm_snow_on.band_values()
 
 sd_difference_values = sfm_snow_depth_values - aso_snow_depth_values
@@ -27,7 +27,6 @@ assert aso_snow_depth.geo_transform == \
 
 
 def mask_to_snow_depth():
-    
     for values in [
         sfm_snow_free_values, 
         sfm_snow_on_values,
